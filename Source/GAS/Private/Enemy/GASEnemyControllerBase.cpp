22c0a5a70167dd05852154c7531d0e9b61487620
Kieran T <kieran123416@gmail.com>
2025-08-01 00:10:57 +0100

    starting AI setup

---
+++ Source/GAS/Private/Enemy/GASEnemyControllerBase.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/GASEnemyControllerBase.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+
+AGASEnemyControllerBase::AGASEnemyControllerBase(const FObjectInitializer& ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	TargetKeyName = "Target";
+	POIKeyName = "PointOfInterest";
+	AIStateKeyName = "State";
+}
+
+void AGASEnemyControllerBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (BehaviorTree)
+	{
+		RunBehaviorTree(BehaviorTree);
+		SetStateAsSpawning();
+	}
+
+}
+
+void AGASEnemyControllerBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+EAIStates AGASEnemyControllerBase::GetAIState() const
+{
+	return static_cast<EAIStates>(Blackboard->GetValueAsEnum(AIStateKeyName));
+}
+
+void AGASEnemyControllerBase::SetStateAsIdle()
+{
+	Blackboard->SetValueAsEnum(AIStateKeyName, static_cast<uint8>(EAIStates::Idle));
+}
+
+void AGASEnemyControllerBase::SetStateAsSpawning()
+{
+}
+
+void AGASEnemyControllerBase::SetStateAsAttacking(UObject* AttackTarget)
+{
+}
+
+void AGASEnemyControllerBase::SetStateAsDying()
+{
+}
---
+++ Source/GAS/Public/Enemy/GASEnemyControllerBase.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "GASEnemyControllerBase.generated.h"
+
+/**
+ *
+ */
+
+UENUM(BlueprintType)
+enum class EAIStates : uint8
+{
+	Idle		UMETA(DisplayName = "Idle"),
+	Spawning	UMETA(DisplayName = "Spawning"),
+	Attacking	UMETA(DisplayName = "Attacking"),
+	Dying		UMETA(DisplayName = "Dead"),
+};
+
+UCLASS()
+class GAS_API AGASEnemyControllerBase : public AAIController
+{
+	GENERATED_BODY()
+public: 
+	AGASEnemyControllerBase(const FObjectInitializer& ObjectInitializer);
+protected:
+	virtual void BeginPlay() override;
+
+public: 
+	virtual void Tick(float DeltaTime) override;
+
+private: 
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Blackboard|Keys", meta = (AllowPrivateAccess = true))
+	FName TargetKeyName;
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Blackboard|Keys", meta = (AllowPrivateAccess = true))
+	FName POIKeyName;
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Blackboard|Keys", meta = (AllowPrivateAccess = true))
+	FName AIStateKeyName;
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Blackboard", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UBehaviorTree> BehaviorTree;
+
+public:
+	UFUNCTION(BlueprintPure)
+	EAIStates GetAIState() const;
+
+	UFUNCTION(BlueprintCallable)
+	void SetStateAsIdle(); 
+	UFUNCTION(BlueprintCallable)
+	void SetStateAsSpawning();
+	UFUNCTION(BlueprintCallable)
+	void SetStateAsAttacking(UObject* AttackTarget);
+	UFUNCTION(BlueprintCallable)
+	void SetStateAsDying();
+
+	
+};

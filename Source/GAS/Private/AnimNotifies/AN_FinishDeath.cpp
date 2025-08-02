df7c91beed5c2a374517b6b0f150a061e6c0916f
Kieran T <kieran123416@gmail.com>
2025-08-02 22:47:38 +0100

    setting up player animations

---
+++ Source/GAS/Private/AnimNotifies/AN_FinishDeath.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifies/AN_FinishDeath.h"
+#include "Characters/GASCharacterBase.h"
+
+UAN_FinishDeath::UAN_FinishDeath()
+{
+	NotifyColor = FColor::Red;
+}
+
+void UAN_FinishDeath::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::Notify(MeshComp, Animation);
+	if(AGASCharacterBase * CharRef = Cast<AGASCharacterBase>(MeshComp->GetOwner()))
+	{
+		CharRef->FinishDying();
+	}
+}
